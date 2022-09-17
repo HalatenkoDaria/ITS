5af6b3f87f581701d6cc53d888fa4991012519a3
Галатенко Дарія ФІТ 16 група <113441359+HalatenkoDaria@users.noreply.github.com>
2022-09-17 15:51:53 +0300

    Add files via upload

---
+++ LB4var2.py
@@ -0,0 +1,7 @@
+import math
+x=float(input("x="))
+a=float(input("a="))
+b=float(input("b"))
+c=float(input("c"))
+Y= math.pow(x,2)+math.log(math.fabs(a),math.e)-math.sin(b)*math.sqrt(math.fabs(c))
+print(Y)
